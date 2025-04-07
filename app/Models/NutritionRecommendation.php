d6b17e86184b878e1d3f0c1c349f04ec7f8b6d78
HelliusYunikaHartono <helliushartono8@gmail.com>
2025-04-07 20:51:44 +0700

    new model rekomendasi nutrisi

---
+++ app/Models/NutritionRecommendation.php
@@ -0,0 +1,15 @@
+<php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NutritionRecommendation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'nutrition', 'ingredients', 'instructions', 'category', 'image'
+    ];
+}
