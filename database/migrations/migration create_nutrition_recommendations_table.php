9b3089bab96fdf8307f5d604c79da434b59c8f4e
HelliusYunikaHartono <helliushartono8@gmail.com>
2025-04-07 20:49:13 +0700

    update database rekomendasi nutrisi

--- database/migrations/migration create_nutrition_recommendations_table.php
+++ database/migrations/migration create_nutrition_recommendations_table.php
@@ -1,13 +1,23 @@
-public function up()
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
 {
-    Schema::create('nutrition_recommendations', function (Blueprint $table) {
-        $table->id();
-        $table->string('name'); // Nama Menu
-        $table->text('nutrition'); // Informasi nutrisi
-        $table->text('ingredients'); // Bahan-bahan
-        $table->text('instructions'); // Cara membuat
-        $table->enum('category', ['pagi', 'siang', 'malam', 'snack']); // Kategori makanan
-        $table->string('image')->nullable(); // Gambar
-        $table->timestamps();
-    });
-}
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('nutrition_recommendations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nama Menu
+            $table->text('nutrition'); // Informasi nutrisi
+            $table->text('ingredients'); // Bahan-bahan
+            $table->text('instructions'); // Cara membuat
+            $table->enum('category', ['pagi', 'siang', 'malam', 'snack']); // Kategori makanan
+            $table->string('image')->nullable(); // Gambar
+            $table->timestamps();
+        });
+    }
+};
