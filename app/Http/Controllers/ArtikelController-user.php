846bcd257f1cfca6a5e57ea4ca8c6fc15a3e9436
DESKTOP-A006P2T\Asus <d.radju25@gmail.com>
2025-04-09 20:02:18 +0700

    controller user (artikel)

---
+++ app/Http/Controllers/ArtikelController-user.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Artikel;
+
+class ArtikelController extends Controller
+{
+    // Tampilkan daftar semua artikel untuk user
+    public function index()
+    {
+        $artikels = Artikel::latest()->get();
+        return view('user.artikel.index', compact('artikels'));
+    }
+
+    // Tampilkan detail satu artikel
+    public function show($id)
+    {
+        $artikel = Artikel::findOrFail($id);
+        return view('user.artikel.show', compact('artikel'));
+    }
+}
