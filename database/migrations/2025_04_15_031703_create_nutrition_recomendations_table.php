b9b3ef8f31dacf1731c042dfdcfbf223dbac2095
HelliusYunikaHartono <helliushartono8@gmail.com>
2025-04-15 10:18:53 +0700

    update database

--- database/migrations/2025_04_15_031703_create_nutrition_recomendations_table.php
+++ database/migrations/2025_04_15_031703_create_nutrition_recomendations_table.php
@@ -1,15 +1,17 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('nutrition_recommendations', function (Blueprint $table) {
+        Schema::create('nutrition_recomendations', function (Blueprint $table) {
             $table->id();
             $table->string('name'); // Nama Menu
             $table->text('nutrition'); // Informasi nutrisi
@@ -19,5 +21,13 @@ return new class extends Migration
             $table->string('image')->nullable(); // Gambar
             $table->timestamps();
         });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nutrition_recomendations');
     }
 };
