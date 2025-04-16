c72fa2ff20fe17ed7f0da59b93799f9b6c5aca8f
nathaniasalsabila28 <nathaniasalsabila8j@gmail.com>
2025-04-16 23:16:54 +0700

    membuat model, controller dan migration

---
+++ app/Http/Controllers/ImmunizationController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Immunization;
+use Illuminate\Http\Request;
+
+class ImmunizationController extends Controller
+{
+    public function index()
+    {
+        $immunizations = Immunization::all();
+        return view('admin.immunizations.index', compact('immunizations'));
+    }
+
+    public function create()
+    {
+        return view('admin.immunizations.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        Immunization::create($request->all());
+
+        return redirect()->route('admin.immunizations.index')->with('success', 'Imunisasi berhasil ditambahkan');
+    }
+
+    public function edit(Immunization $immunization)
+    {
+        return view('admin.immunizations.edit', compact('immunization'));
+    }
+
+    public function update(Request $request, Immunization $immunization)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $immunization->update($request->all());
+
+        return redirect()->route('admin.immunizations.index')->with('success', 'Imunisasi berhasil diupdate');
+    }
+
+    public function destroy(Immunization $immunization)
+    {
+        $immunization->delete();
+        return back()->with('success', 'Imunisasi berhasil dihapus');
+    }
+}
