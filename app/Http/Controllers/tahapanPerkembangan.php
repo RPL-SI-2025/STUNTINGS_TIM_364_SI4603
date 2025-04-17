d92af0376dc41fd88f8efaa4409ec3c6578b1f4f
afrafaradilahh <afraradilah@student.telkomuniversity.ac.id>
2025-04-17 20:28:28 +0700

    controller admin

---
+++ app/Http/Controllers/tahapanPerkembangan.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tahapanPerkembangan;
+use Illuminate\Http\Request;
+
+class tahapanPerkembanganController extends Controller
+{
+    public function index()
+    {
+        $tahapanPerkembangan = tahapanPerkembangan::all();
+        return view('admin.tahapanPerkembangan.index', compact('tahapanPerkembangan'));
+    }
+
+    public function create()
+    {
+        return view('admin.tahapanPerkembangan.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kategori' => 'required',
+            'tahapan' => 'required',
+            'deskripsi' => 'required',
+        ]);
+
+        tahapanPerkembangan::create($request->all());
+
+        return redirect()->route('tahapanPerkembangan.index')->with('success', 'Data berhasil ditambahkan');
+    }
+}
